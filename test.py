376ac3707d46be954e77951f30f3f17540bfaf02
Cecil Coupe <ccoupe@bronco>
2023-01-19 22:04:02 -0700

    test.py gets --host and --port arguments Port default to 5449 which is the test docker container.

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import sys
 import websocket  # pip3 install websocket-client
 import base64
 import json
+import argparse
 
 def get_name(uri, bfr):
   ws = websocket.WebSocket()
@@ -18,11 +19,18 @@ def get_name(uri, bfr):
 def main():
   # hardcoded for me and my machines and network.
   #host = 'mini.local'
-  host = '192.168.1.2'  # bronco.local won't work! BUG somewhere 
+  host = '192.168.1.45'  # bronco.local won't work! BUG somewhere 
   port = 5439
-  #uri = f'ws://{host}:{port}'
   uri = f'ws://{host}:{port}/Cnn_Shapes'
+  ap = argparse.ArgumentParser()
+  ap.add_argument("--host", action='store', type=str, default='192.168.1.2',
+    nargs='?', help="host ip of mlshapes server")
+  ap.add_argument("-p", "--port", action='store', type=int, default='5439',
+    nargs='?', help="server port number, 5439 is default")
+  args = vars(ap.parse_args())
 
+  uri = f'ws://{args["host"]}:{args["port"]}/Cnn_Shapes'
+  print('using', uri)
   fp = './test_image/person.jpg'
   f = open(fp, 'rb')
   img = f.read()
